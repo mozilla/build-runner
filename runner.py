6891c6dbbb3f69f85538e7c760349000f243dd54
Ian Connolly <iconnolly@mozilla.com>
2014-07-30 18:18:38 -0700

    Add un-integrated dependency code

--- runner.py
+++ runner.py
@@ -3,6 +3,8 @@
 import os
 import time
 import subprocess
+import copy
+import itertools
 import re
 import sys
 from ConfigParser import RawConfigParser
@@ -153,6 +155,125 @@ def list_directory(dirname):
 
 def get_task_name(taskfile):
     return taskfile.split('-', 1)[1].split('.', 1)[0]
+
+
+class CycleError(Exception):
+    pass
+
+
+class DependencyDoesNotExistError(Exception):
+    pass
+
+
+class TaskGraph(object):
+    def __init__(self, taskconfigs):
+        self._nodes = {}
+        for task in taskconfigs:
+            self._nodes[task.name] = task
+            self._refresh()
+
+        missing = self._missing_tasks()
+        if missing:
+            raise DependencyDoesNotExistError("The following are depended on "
+                                              "but do not exist: "
+                                              + ", ".join(missing))
+
+    def _refresh(self):
+        for node in self._nodes.values():
+            for sd in node.stated_dependencies:
+                if sd in self._nodes and sd not in node.dependencies:
+                    node.dependencies.add(self._nodes[sd])
+
+    def _missing_tasks(self):
+        """List of missing tasks (ie. unfulfilled dependencies)"""
+        lst = [node._missing_dependencies() for node in self._nodes.values()]
+        return list(set(itertools.chain.from_iterable(lst)))  # flatten
+
+    def sequential_ordering(self):
+        """Topological sort, ignores parallelisation possibilities
+        Algorithm is Kahn (1962),
+        http://en.wikipedia.org/wiki/Topological_sorting#Algorithms
+        """
+
+        to_ret = []
+        graph = copy.deepcopy(self._nodes.values())
+        no_inc_edges = self._start_nodes(graph)
+
+        while no_inc_edges:
+            n = no_inc_edges.pop()
+            to_ret.append(n.name)
+            print to_ret
+            for m in self._nodes_with_edges_from(graph, n):
+                self._remove_edge(graph, n, m)
+                if not set(self._nodes_with_edges_to(graph, m)) - set([n]):
+                    no_inc_edges.append(m)
+
+        if self._has_edges(graph):
+            # we've got a cycle in our graph!
+            raise CycleError("Graph of task dependencies has cycles")
+
+        to_ret.reverse()  # because we point TO our dependents
+        return to_ret
+
+    @staticmethod
+    def _start_nodes(graph):
+        """Returns the nodes in the graph with no dependencies"""
+        return [n for n in graph
+                if not TaskGraph._nodes_with_edges_to(graph, n)]
+
+    @staticmethod
+    def _has_edges(graph):
+        """Returns True if there are any edges remaining in the graph,
+        False otherwise"""
+
+        for node in graph:
+            if node.dependencies:
+                return True
+        return False
+
+    @staticmethod
+    def _nodes_with_edges_to(graph, n):
+        return [m for m in graph if m is not n and n in m.dependencies]
+
+    @staticmethod
+    def _nodes_with_edges_from(graph, n):
+        return list(n.dependencies)
+
+    @staticmethod
+    def _remove_edge(graph, n, m):
+        """Remove the edge from n to m"""
+        if m not in n.dependencies:
+            return
+
+        n.dependencies.remove(m)
+
+    def __str__(self):
+        return ", ".join(map(str, self._nodes.values()))
+
+
+class TaskConfig(object):
+    def __init__(self, name, dependencies):
+        self.name = name
+        self.stated_dependencies = set(dependencies) - set(self.name)
+        self.dependencies = set()
+
+    @classmethod
+    def fromtuple(cls, pair):
+        return cls(pair[0], pair[1])
+
+    @classmethod
+    def fromdict(cls, mapping):
+        return cls(mapping['name'], mapping['dependencies'])
+
+    def _missing_dependencies(self):
+        return list(set(self.stated_dependencies)
+                    - set([d.name for d in self.dependencies]))
+
+    def __str__(self):
+        return "({}, {}, {})".format(self.name,
+                                     self.stated_dependencies,
+                                     self.dependencies)
+
 
 
 def process_taskdir(config, dirname):
