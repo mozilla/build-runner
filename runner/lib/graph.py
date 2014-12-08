9b8d53730c7cd145ccf581b940b38d756d5da251
Morgan Phillips <winter2718@gmail.com>
2014-12-08 17:23:18 +0000

    Add test for tasks sorted by name.

--- runner/lib/graph.py
+++ runner/lib/graph.py
@@ -48,6 +48,8 @@ class TaskGraph(object):
 
         to_ret = []
         graph = copy.deepcopy(self._nodes.values())
+        # our starting nodes should be sorted for users who expect init
+        # [runlevel] type behavior
         no_inc_edges = sorted(self._start_nodes(graph), key=lambda x: x.name)
         while no_inc_edges:
             n = no_inc_edges.pop()
@@ -55,7 +57,7 @@ class TaskGraph(object):
             for m in self._nodes_with_edges_from(graph, n):
                 self._remove_edge(graph, n, m)
                 if not set(self._nodes_with_edges_to(graph, m)) - set([n]):
-                    no_inc_edges.add(m)
+                    no_inc_edges.append(m)
 
         if self._has_edges(graph):
             # we've got a cycle in our graph!
--- tests/pre-post-hook.py
+++ tests/pre-post-hook.py
@@ -8,7 +8,7 @@ A simple pre/post hook which writes data to a file
 import sys
 
 # protect ourselves from unintentional runs during test discovery
-if sys.argv[1] == "runner-test":
+if len(sys.argv) > 1 and sys.argv[1] == "runner-test":
     with open(sys.argv[2], 'a') as logfile:
         logfile.write(sys.argv[3])
         logfile.write('\n')
